3902f2ba2e985d4c4319e6f08e6c4883c939beeb
Sofia Lunkes <sofia.big@live.com>
2021-05-22 03:03:19 -0300

    Perfil agora contem configuracao pra desativar ou ativar participacao na lista de revisao. A ideia é utilizar quando estiver de férias ou saindo do projeto

--- app/components/FlashMessages.js
+++ app/components/FlashMessages.js
@@ -2,16 +2,23 @@ import React, { useEffect } from "react";
 
 function FlashMessages(props) {
   let cssBehavior = "success";
+  let behavior = props.behavior;
   if (props.behavior == "error") {
     cssBehavior = "danger";
   }
   if (props.behavior == "info") {
     cssBehavior = "info";
   }
+  if (props.behavior == "default") {
+    cssBehavior = "info";
+  }
 
   function createMarkup(msg) {
-    if (cssBehavior == "info") {
+    if (cssBehavior == "info" && !behavior == "default") {
       return { __html: `${msg} &#9996` };
+    }
+    if (cssBehavior == "info") {
+      return { __html: `${msg}` };
     }
     if (cssBehavior == "danger") {
       return { __html: `${msg} &#129488` };
--- app/components/Profile.js
+++ app/components/Profile.js
@@ -2,39 +2,48 @@ import React, { useEffect, useContext } from "react";
 import { useImmer, useImmerReducer } from "use-immer";
 import { useParams } from "react-router-dom";
 import Page from "./Page";
+import DispatchContext from "../DispatchContext";
 import StateContext from "../StateContext";
+import LoadingDotsIcon from "./LoadingDotsIcon";
 import Axios from "axios";
 
 function Profile() {
   const appState = useContext(StateContext);
+  const appDispatch = useContext(DispatchContext);
 
   const initialState = {
     submitCount: 0,
     profileData: {
       name: "...",
       email: "...",
-      nickname: "...",
+      nickname: useParams().username,
       scope: "...",
       avatar: "https://secure.gravatar.com/avatar/3ea9b4a66b36358f4caa9416c607a74d.jpg?s=512&d=https%3A%2F%2Fa.slack-edge.com%2Fdf10d%2Fimg%2Favatars%2Fava_0003-512.png",
       active: false
-    }
+    },
+    isFetching: true,
+    isSaving: false,
+    sendCount: 0
   };
 
   function ourReducer(draft, action) {
     switch (action.type) {
       case "fetchComplete":
-        console.log("entrou aqui");
-        console.log(action.value);
         draft.profileData.name = action.value.name;
         draft.profileData.email = action.value.email;
-        draft.profileData.nickname = action.value.nickname;
         draft.profileData.scope = action.value.scope;
         draft.profileData.active = action.value.active;
         if (action.value.avatar != null) {
           draft.profileData.avatar = action.value.avatar;
         }
+        draft.isFetching = false;
+        return;
       case "activeImmediately":
-        draft.profileData.value = action.value;
+        draft.profileData.active = action.value;
+        draft.sendCount++;
+        return;
+      case "activeRollback":
+        draft.profileData.active = !action.value;
     }
   }
 
@@ -42,43 +51,73 @@ function Profile() {
 
   useEffect(() => {
     const ourRequest = Axios.CancelToken.source();
+
     async function fetchUser() {
       try {
         const response = await Axios.get(`/users/${appState.user.nickname}`, { headers: { Authorization: appState.user.token } }, { cancelToken: ourRequest.token });
         if (response.data) {
           dispatch({ type: "fetchComplete", value: response.data });
+        } else {
+          appDispatch({ type: "flashMessage", behavior: "error", value: "Alguma coisa aconteceu aqui." });
         }
       } catch (e) {
         console.log(e.response);
       }
     }
     fetchUser();
-
     return () => {
       ourRequest.cancel();
     };
   }, []);
 
-  function handleActiveRevisor(params) {}
+  useEffect(() => {
+    const ourRequest = Axios.CancelToken.source();
+    const options = {
+      headers: { Authorization: appState.user.token },
+      cancelToken: ourRequest.token
+    };
+    if (state.sendCount > 0) {
+      async function updateStatus() {
+        try {
+          const response = await Axios.post(`/users/${appState.user.nickname}/status`, state.profileData, options);
+        } catch (e) {
+          if (e.response.data.type == "user_with_totem") {
+            appDispatch({ type: "flashMessage", behavior: "default", value: "Você possui um totem, repasse antes de sair da lista." });
+          }
+          dispatch({ type: "activeRollback", value: state.profileData.active });
+        }
+      }
+
+      updateStatus();
+    }
+
+    return () => {
+      ourRequest.cancel();
+    };
+  }, [state.sendCount]);
+
+  if (state.isFetching)
+    return (
+      <Page title="...">
+        <LoadingDotsIcon />
+      </Page>
+    );
+
   return (
     <Page title="Perfil">
-      <h2>
-        <img className="avatar-small" src={state.profileData.avatar} /> Configurações
-      </h2>
+      <div className="row">
+        <div className="col-4">
+          <img className="avatar-regular" src={state.profileData.avatar} /> Configurações
+        </div>
 
-      <div className="card">
-        <div className="card-body">
-          <div className="form-check form-switch">
-            <input onChange={e => dispatch(e)} className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked={state.profileData.active} />
-            <label className="form-check-label" htmlFor="flexSwitchCheckChecked">
-              Participar da lista
-            </label>
-          </div>
-          <div className="form-check form-switch">
-            <input onChange={e => dispatch(e)} className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked={state.profileData.active} />
-            <label className="form-check-label" htmlFor="flexSwitchCheckChecked">
-              Participar da lista
-            </label>
+        <div className="card col-6">
+          <div className="card-body">
+            <div className="form-check form-switch">
+              <input onChange={e => dispatch({ type: "activeImmediately", value: e.target.checked })} className="form-check-input" type="checkbox" id="isInReviewList" checked={state.profileData.active} />
+              <label className="form-check-label" htmlFor="isInReviewList">
+                Participar da revisão
+              </label>
+            </div>
           </div>
         </div>
       </div>

0e888203af0d4c9208a3e07cf1407b30e6dcf40d
Sofia Lunkes <sofia.big@live.com>
2021-05-27 23:53:29 -0300

    Preenchendo os campos do formulario com valor no objeto. Criado uma pagina com alguns detalhes de totens repassados e ultimas mudancas, valores fixos.

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,8 +8,38 @@ import { useImmer } from "use-immer";
 
 function Home() {
   return (
-    <Page title="HomePage">
-      <h1> Titulo!!</h1>
+    <Page title="Home">
+      <div className="row">
+        <h1> Ultimas mudanças criadas</h1>
+        <div>
+          <ul>
+            <li>
+              odete-account v1.44.1 atualização para 27/05/2021 as 8h30 por Sofia Lunkes da Silva. <small>criado às 17:50 26/07/2021 </small>
+            </li>
+            <li>
+              cb-partner-events v1.9.3 atualização para 28/05/2021 as 8h30 por Antonio Pereira. <small>criado às 10:50 27/07/2021 </small>
+            </li>
+            <li>
+              cb-payment-account v1.2 atualização para 28/05/2021 as 8h30 por Alan Magalhães Lira. <small>criado às 10:54 27/07/2021 </small>
+            </li>
+            <li>
+              odete-proposalengine v1.9.43 atualização para 28/05/2021 as 8h30 por Eduardo Miani. <small>criado às 11:00 27/07/2021 </small>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="row">
+        <h1 className="col-12"> Ultimos totens</h1>
+        <div>
+          <ul>
+            <li>Totem Vermelho passou de Luiza para Fernando</li>
+            <li>Totem Azul passou de José para Sofia</li>
+            <li>Totem Roxo passou de Vitor para Lucas</li>
+            <li>Totem Verde passou de Marcelo para Antonio</li>
+          </ul>
+        </div>
+      </div>
     </Page>
   );
 }
--- app/components/promotion/Promotion.js
+++ app/components/promotion/Promotion.js
@@ -1,27 +1,213 @@
 import React, { useContext, useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
+import { useImmerReducer } from "use-immer";
 import Page from "../Page";
 
 function Promotion() {
+  const initialState = {
+    service: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    date: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    squad: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    repository: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    version: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    motivation: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    dateStart: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    dateFinish: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    responsible: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    racf: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    jira: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    description: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    component: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    impact: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    probability: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    rollbackPlan: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    submitCount: 0
+  };
+
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "serviceImmediately":
+        draft.service.hasErrors = false;
+        draft.service.value = action.value;
+        return;
+      case "dateImmediately":
+        draft.date.hasErrors = false;
+        draft.date.value = action.value;
+        return;
+      case "squadImmediately":
+        draft.squad.hasErrors = false;
+        draft.squad.value = action.value;
+        return;
+      case "repositoryImmediately":
+        draft.repository.hasErrors = false;
+        draft.repository.value = action.value;
+        return;
+      case "versionImmediately":
+        draft.version.hasErrors = false;
+        draft.version.value = action.value;
+        return;
+      case "motivationImmediately":
+        draft.motivation.hasErrors = false;
+        draft.motivation.value = action.value;
+        return;
+      case "dateStartImmediately":
+        draft.dateStart.hasErrors = false;
+        draft.dateStart.value = action.value;
+        return;
+      case "dateFinishImmediately":
+        draft.dateFinish.hasErrors = false;
+        draft.dateFinish.value = action.value;
+        return;
+      case "responsibleImmediately":
+        draft.responsible.hasErrors = false;
+        draft.responsible.value = action.value;
+        return;
+      case "racfImmediately":
+        draft.racf.hasErrors = false;
+        draft.racf.value = action.value;
+        return;
+      case "jiraImmediately":
+        draft.jira.hasErrors = false;
+        draft.jira.value = action.value;
+        return;
+      case "descriptionImmediately":
+        draft.description.hasErrors = false;
+        draft.description.value = action.value;
+        return;
+      case "componentsImmediately":
+        draft.component.hasErrors = false;
+        draft.component.value = action.value;
+        return;
+      case "impactImmediately":
+        draft.impact.hasErrors = false;
+        draft.impact.value = action.value;
+        return;
+      case "probabilityImmediately":
+        draft.probability.hasErrors = false;
+        draft.probability.value = action.value;
+        return;
+      case "rollbackImmediately":
+        draft.rollbackPlan.hasErrors = false;
+        draft.rollbackPlan.value = action.value;
+        return;
+      case "submitForm":
+        console.log(draft);
+        draft.submitCount++;
+        return;
+    }
+  }
+
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
+
+  useEffect(() => {
+    if (state.submitCount) {
+      console.log(state);
+    }
+  }, [state.submitCount]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: "submitForm" });
+  }
+
   return (
     <Page title="Promotion">
       <h1>Promotion</h1>
       <div className="card">
         <div className="card-header">Descreva a mudança</div>
         <div className="card-body">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group row">
               <label htmlFor="inputServico" className="col-sm-2 col-form-label">
                 Serviço
               </label>
               <div className="col-sm-5">
-                <input type="text" className="form-control" id="inputServico" placeholder="ex. cb-cbzito" />
+                <input
+                  onChange={e => {
+                    dispatch({ type: "serviceImmediately", value: e.target.value });
+                  }}
+                  type="text"
+                  className="form-control"
+                  id="inputServico"
+                  placeholder="ex. cb-cbzito"
+                />
               </div>
               <label htmlFor="dataMudanca" className="col-sm-1 col-form-label">
                 Data
               </label>
               <div className="col-sm-3">
-                <input type="date" className="form-control" id="dataMudanca" />
+                <input
+                  type="date"
+                  onChange={e => {
+                    dispatch({ type: "dateImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                  id="dataMudanca"
+                />
               </div>
             </div>
             <div className="form-group row">
@@ -29,7 +215,15 @@ function Promotion() {
                 Squad
               </label>
               <div className="col-sm-9">
-                <input type="text" className="form-control" id="inputSquad" placeholder="Assistir, Conectar, Cross, Conectar.." />
+                <input
+                  type="text"
+                  onChange={e => {
+                    dispatch({ type: "squadImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                  id="inputSquad"
+                  placeholder="Assistir, Conectar, Cross, Conectar.."
+                />
               </div>
             </div>
             <div className="form-group row">
@@ -37,7 +231,15 @@ function Promotion() {
                 Repositório
               </label>
               <div className="col-sm-9">
-                <input type="text" className="form-control" id="inputRepositorio" placeholder="/gif-odete/backend/odete-account " />
+                <input
+                  type="text"
+                  onChange={e => {
+                    dispatch({ type: "repositoryImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                  id="inputRepositorio"
+                  placeholder="/gif-odete/backend/odete-account "
+                />
               </div>
             </div>
             <div className="form-group row">
@@ -45,7 +247,15 @@ function Promotion() {
                 Versão
               </label>
               <div className="col-sm-9">
-                <input type="text" className="form-control" id="inputVersao" placeholder="1.4.2" />
+                <input
+                  type="text"
+                  onChange={e => {
+                    dispatch({ type: "versionImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                  id="inputVersao"
+                  placeholder="1.4.2"
+                />
               </div>
             </div>
             <div className="form-group row">
@@ -53,7 +263,15 @@ function Promotion() {
                 Motivação
               </label>
               <div className="col-sm-9">
-                <input type="text" className="form-control" id="inputMotivacao" placeholder="Ajuste de data de evento de payload" />
+                <input
+                  type="text"
+                  onChange={e => {
+                    dispatch({ type: "motivationImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                  id="inputMotivacao"
+                  placeholder="Ajuste de data de evento de payload"
+                />
               </div>
             </div>
             <div className="form-group row">
@@ -61,13 +279,27 @@ function Promotion() {
                 Início
               </label>
               <div className="col-sm-4">
-                <input type="datetime-local" className="form-control" id="inputInicio" />
+                <input
+                  type="datetime-local"
+                  onChange={e => {
+                    dispatch({ type: "dateStartImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                  id="inputInicio"
+                />
               </div>
               <label htmlFor="inputTermino" className="col-sm-2 col-form-label">
                 Término
               </label>
               <div className="col-sm-3">
-                <input type="datetime-local" className="form-control" id="inputTermino" />
+                <input
+                  type="datetime-local"
+                  onChange={e => {
+                    dispatch({ type: "dateFinishImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                  id="inputTermino"
+                />
               </div>
             </div>
             <div className="form-group row">
@@ -75,22 +307,45 @@ function Promotion() {
                 Responsável
               </label>
               <div className="col-sm-4">
-                <input type="text" className="form-control" id="inputResponsavel" placeholder="Jane Foster" />
+                <input
+                  type="text"
+                  onChange={e => {
+                    dispatch({ type: "responsibleImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                  id="inputResponsavel"
+                  placeholder="Jane Foster"
+                />
               </div>
               <label htmlFor="inputRacf" className="col-sm-2 col-form-label">
                 RACF
               </label>
               <div className="col-sm-3">
-                <input type="text" className="form-control" id="inputRacf" placeholder="RACF" />
+                <input
+                  type="text"
+                  onChange={e => {
+                    dispatch({ type: "racfImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                  id="inputRacf"
+                  placeholder="RACF"
+                />
               </div>
             </div>
-            <div className="form-group row"></div>
             <div className="form-group row">
               <label htmlFor="inputJira" className="col-sm-2 col-form-label">
                 JIRA
               </label>
               <div className="col-sm-9">
-                <input type="text" className="form-control" id="inputJira" placeholder="https://projetoodete.atlassian.net/browse/CB-00001" />
+                <input
+                  type="text"
+                  onChange={e => {
+                    dispatch({ type: "jiraImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                  id="inputJira"
+                  placeholder="https://projetoodete.atlassian.net/browse/CB-00001"
+                />
               </div>
             </div>
             <div className="form-group row">
@@ -98,7 +353,15 @@ function Promotion() {
                 Descricao
               </label>
               <div className="col-sm-9">
-                <input type="text" className="form-control" id="inputDescricao" placeholder="Descricao do que foi alterado" />
+                <input
+                  type="text"
+                  onChange={e => {
+                    dispatch({ type: "descriptionImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                  id="inputDescricao"
+                  placeholder="Descricao do que foi alterado"
+                />
               </div>
             </div>
             <div className="form-group row">
@@ -106,7 +369,15 @@ function Promotion() {
                 Componentes
               </label>
               <div className="col-sm-9">
-                <input type="text" className="form-control" id="inputComponentes" placeholder="Nenhum" />
+                <input
+                  type="text"
+                  onChange={e => {
+                    dispatch({ type: "componentsImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                  id="inputComponentes"
+                  placeholder="Nenhum"
+                />
               </div>
             </div>
             <div className="form-group row">
@@ -114,7 +385,14 @@ function Promotion() {
                 Potencial Impacto
               </label>
               <div className="col-sm-4">
-                <select id="selectImpacto" className="form-control">
+                <select
+                  id="selectImpacto"
+                  value={state.impact.value}
+                  onChange={e => {
+                    dispatch({ type: "impactImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                >
                   <option value="nenhum">Nenhum</option>
                   <option value="baixo">Baixo</option>
                   <option value="medio">Médio</option>
@@ -125,7 +403,14 @@ function Promotion() {
                 Probabilidade
               </label>
               <div className="col-sm-3">
-                <select id="selectProbabilidade" className="form-control">
+                <select
+                  id="selectProbabilidade"
+                  value={state.probability.value}
+                  onChange={e => {
+                    dispatch({ type: "probabilityImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                >
                   <option value="nenhum">Nenhum</option>
                   <option value="baixo">Baixo</option>
                   <option value="medio">Médio</option>
@@ -138,8 +423,21 @@ function Promotion() {
                 Plano de Rollback
               </label>
               <div className="col-sm-9">
-                <input type="text" className="form-control" id="inputRollback" placeholder="Nenhum" />
+                <input
+                  type="text"
+                  onChange={e => {
+                    dispatch({ type: "rollbackImmediately", value: e.target.value });
+                  }}
+                  className="form-control"
+                  id="inputRollback"
+                  placeholder="Nenhum"
+                />
               </div>
+            </div>
+            <div className="form-group row">
+              <button type="submit" className="col-4 offset-4 py-2 mt-4 btn btn-lg btn-success">
+                Criar promotion
+              </button>
             </div>
           </form>
         </div>
